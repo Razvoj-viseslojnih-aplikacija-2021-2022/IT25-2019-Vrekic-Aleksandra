6273f9d57f0266d1e7615847bc224141fe6ca936
AleksandraVrekic <aleksandraavrekic@gmail.com>
2022-09-19 09:48:20 +0200

    backend komitovanje

--- src/main/java/rppstart/ctrls/HelloRestController.java
+++ src/main/java/rppstart/ctrls/HelloRestController.java
@@ -1,8 +1,10 @@
 package rppstart.ctrls;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class HelloRestController {
 
